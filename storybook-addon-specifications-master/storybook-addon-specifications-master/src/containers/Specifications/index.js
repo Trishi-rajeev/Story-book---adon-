29ccb2caf30c8798b35d566db49a797643ff7379
Adonis <trishirajeev1234@gmail.com>
2026-05-23 20:51:02 +0530

    Add files via upload

---
+++ storybook-addon-specifications-master/storybook-addon-specifications-master/src/containers/Specifications/index.js
@@ -0,0 +1,38 @@
+import React, {Component} from "react";
+import SpecificationsComponent from "../../components/Specifications/";
+import {EVENT_ID} from "../../";
+
+
+export default class Specifications extends Component {
+  constructor(props, ...args) {
+    super(props, ...args);
+    this.state = { storyName: null, results: { wrongResults: [], goodResults: [] } };
+    this._listener = ({ asyncResultsUpdate, storyName, results }) => {
+      if (asyncResultsUpdate) {
+        if (storyName === this.state.storyName) {
+          this.setState({ results });
+        }
+      } else {
+        this.setState({ storyName, results });
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.props.channel.on(EVENT_ID, this._listener);
+    this.props.api.on("story", (data) => this.setState({ storyName: null, results: { wrongResults: [], goodResults: [] } }));
+  }
+
+  componentWillUnmount() {
+    this.props.channel.removeListener(EVENT_ID, this._listener);
+  }
+
+  render() {
+    const results = this.state.results;
+    const { active } = this.props;
+
+    return active ? (
+      <SpecificationsComponent results={results} />
+    ) : null;
+  }
+}
